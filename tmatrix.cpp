f6f354fa070fdf855251c40a428424623b349567
Mirovengil <Mirivengil@gmail.com>
2024-12-09 20:05:05 +0300

    Реализовал класс матрицы.

--- tmatrix.cpp
+++ tmatrix.cpp
@@ -1,6 +1,107 @@
 #include "tmatrix.h"
+#include <stdexcept>
+
+// TMatrix's methods
+
+void TMatrix::checkIfPointIsInMatrix(TPoint point)
+{
+    if (!pointIsInMatrix(point))
+        throw std::range_error("TMatrix : point is out of matrix!");
+}
+
+TMatrixPoint &TMatrix::getPoint(TPoint point)
+{
+    return data[point.getY()][point.getX()];
+}
 
 TMatrix::TMatrix()
 {
 
+}
+
+bool TMatrix::pointIsInMatrix(TPoint point)
+{
+    int N = data.size();
+
+    if (N == 0)
+       return false;
+
+    int M = data[0].size();
+
+    return ((N > point.getY()) && (M > point.getX()));
+}
+
+bool TMatrix::isVisited(TPoint point)
+{
+    checkIfPointIsInMatrix(point);
+    return getPoint(point).isVisited();
+}
+
+void TMatrix::visit(TPoint point)
+{
+    checkIfPointIsInMatrix(point);
+    getPoint(point).visit();
+}
+
+void TMatrix::unvisit(TPoint point)
+{
+    checkIfPointIsInMatrix(point);
+    getPoint(point).unvisit();
+}
+
+void TMatrix::setValue(TPoint point, type value)
+{
+    checkIfPointIsInMatrix(point);
+    getPoint(point).setValue(value);
+}
+
+void TMatrix::resize(int N, int M)
+{
+    data.resize(N);
+    for (int i = 0; i < N; ++i)
+        data[i].resize(M);
+    unvisit();
+}
+
+void TMatrix::unvisit()
+{
+    for (int i = 0; i < data.size(); ++i)
+        for (int j = 0; j < data[i].size(); ++j)
+        {
+            TPoint point(j, i);
+            getPoint(point).unvisit();
+        }
+}
+
+type TMatrix::getValue(TPoint point)
+{
+    checkIfPointIsInMatrix(point);
+    return getPoint(point).getValue();
+}
+
+// TMatrixPoint's methods
+
+void TMatrixPoint::setValue(type value)
+{
+    this->value = value;
+}
+
+type TMatrixPoint::getValue()
+{
+    return value;
+}
+
+void TMatrixPoint::visit()
+{
+    visited = true;
+}
+
+void TMatrixPoint::unvisit()
+{
+    visited = false;
+}
+
+bool TMatrixPoint::isVisited()
+{
+    return visited;
 }
--- tmatrix.h
+++ tmatrix.h
@@ -1,11 +1,42 @@
 #ifndef TMATRIX_H
 #define TMATRIX_H
 
+#include <vector>
+#include <stdint.h>
+#include "tpoint.h"
+
+typedef int16_t type;
+
+class TMatrixPoint
+{
+private:
+    type value;
+    bool visited;
+public:
+    void setValue(type value);
+    type getValue();
+    void visit();
+    void unvisit();
+    bool isVisited();
+};
 
 class TMatrix
 {
+private:
+    std::vector <std::vector <TMatrixPoint>> data;
+    void checkIfPointIsInMatrix(TPoint point);
+    TMatrixPoint &getPoint(TPoint point);
 public:
     TMatrix();
+    bool pointIsInMatrix(TPoint point);
+    bool isVisited(TPoint point);
+    void visit(TPoint point);
+    void unvisit(TPoint point);
+    void setValue(TPoint point, type value);
+    void resize(int N, int M);
+    void unvisit();
+    type getValue(TPoint point);
+
 };
 
 #endif // TMATRIX_H
